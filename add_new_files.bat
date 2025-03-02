c455d0701683e043b390f48686e981b15481a597
smallcolorsese <smallcolorsese@gmail.com>
2025-03-01 20:30:53 -0500

    Add new files

--- add_new_files.bat
+++ add_new_files.bat
@@ -5,12 +5,8 @@ REM 65001 代表 UTF-8 编码，>nul 让其不显示切换编码的消息
 cd /d %~dp0
 REM 切换到当前脚本所在目录，确保 Git 命令在正确的 Git 仓库目录中执行
 
-echo 正在添加所有更改...
 git add .
-echo.
-echo 正在提交更改...
 git commit -m "Add new files"
-echo.
 echo 正在推送到远程仓库...
 git push origin main
 echo.
